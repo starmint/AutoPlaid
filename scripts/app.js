2a942e2eb9eb1899f06fa2b80f42ee181f84e5ba
Cameron Ray <cameronray@openarc.net>
2014-12-16 16:51:47 -0500

    add distance slider

--- scripts/app.js
+++ scripts/app.js
@@ -11,12 +11,14 @@ var CANVAS = (function ($) {
 
 		for (var i = 3; i >= 0; i--) {
 			var width = $("#vert_1_size").val();
+			var distance = $("#vert_1_distance").val();
 			ctx.fillStyle = "black";
-			ctx.fillRect(10, (i * 20) + (width * i), 100, width);
+			ctx.fillRect(0, (i * distance) + (width * i), 100, width);
 
 			width = $("#horiz_1_size").val();
+			distance = $("#horiz_1_distance").val();
 			ctx.fillStyle = "white";
-			ctx.fillRect((i * 50) + (width * i), 10, width, 100);
+			ctx.fillRect((i * distance) + (width * i), 0, width, 100);
 		};
 	}
 
