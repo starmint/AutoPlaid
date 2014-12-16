e710e24d7e12f95324f2be29e9977492f7a6926a
Cameron Ray <cameronray@openarc.net>
2014-12-16 16:22:59 -0500

    some canvas interaction

--- scripts/app.js
+++ scripts/app.js
@@ -2,20 +2,39 @@ var CANVAS = (function ($) {
 	var canvas = {};
 	var ctx = {};
 
+	function canvas_redraw () {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		ctx.fillStyle = "green";
+		ctx.fillRect(10,10,$("#horiz_1_size").val(),100);
+		ctx.fillStyle = "red";
+		ctx.fillRect(10,10,100,$("#vert_1_size").val());
+	}
+
+	function init_form () {
+		$("#horiz_1_size").change(function (event) {
+			canvas_redraw();
+		});
+		$("#vert_1_size").change(function (event) {
+			canvas_redraw();
+		});
+	}
+
 	function init_canvas () {
 		canvas = document.getElementById("area");
 		ctx = canvas.getContext("2d");
-		ctx.fillStyle = "green";
-		ctx.fillRect(10,10,100,100);
+		canvas_redraw();
 	}
 
 	return {
 		initialize: function () {
 			init_canvas();
+			init_form();
 		}
 	};
 })(jQuery);
 
+
+//This function gets called automatically on page load. 
 (function ($, cv, undefined) {
 	$(document).ready(function() {
 		initialize();
